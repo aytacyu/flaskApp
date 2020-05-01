4645b3a057ea846d790a31ea3ac5ad0f51456219
aytacyu <aytacyu@gmail.com>
2020-05-01 17:00:45 +0100

    add bookmark page

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -18,5 +18,10 @@ def index():
     return render_template('index.html', text=['Welcome,...', 'Secondly,...', 'Also,...'], user=User("John", "Wick"))
 
 
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
