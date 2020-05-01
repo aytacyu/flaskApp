4afee5d9bd498462156403154202b9a743382dc9
aytacyu <aytacyu@gmail.com>
2020-05-01 16:51:08 +0100

    app init

---
+++ thermos/thermos.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+class User:
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+
+    def initials(self):
+        return f"{self.name[0]}. {self.lastname[0]}."
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', text=['Welcome,...', 'Secondly,...', 'Also,...'], user=User("John", "Wick"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
